eab8e39d84167b9d3b88412792cd12a9d25db5d1
Alexey Huralnyk <huralnyk@edsson.com>
2016-04-19 13:36:19 +0300

    SpriteKit Demo

---
+++ Games-Sensors-and-Media/SpriteKit/SpriteKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteKit
+//
+//  Created by Alexey Huralnyk on 4/17/16.
+//  Copyright © 2016 Alexey Huralnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Games-Sensors-and-Media/SpriteKit/SpriteKit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpriteKit
+//
+//  Created by Alexey Huralnyk on 4/17/16.
+//  Copyright © 2016 Alexey Huralnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
